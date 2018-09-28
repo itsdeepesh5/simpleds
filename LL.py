72c1cd28aecf5fd99c80d6ebfac49511c6297d55
Deepesh K Rana <deepesh.rana@gmail.com>
2018-09-28 15:00:04 -0400

    Update LL.py

--- LL.py
+++ LL.py
@@ -10,28 +10,28 @@ class Node:
     self.prev=None
     self.value=None
     self.next=None
-    
+
 class LL:
   def __init__(self):
     self.node= Node()
     self.head=self.node
-  
+
   def append(self, x):
     print("insert",x)
     new=Node()
     new.value=x
-    
+
     if self.head.value is None:
       self.head.prev=None
       self.head.value=x
       self.head.next=None
     else:
-      current= self.head  
+      current= self.head
       while(current is not None):
         if current.next is None:
           current.next = new
           new.prev= current
-          return True		  
+          return True
         current = current.next
 
   def insertAt(self, x):
@@ -43,44 +43,68 @@ class LL:
       self.head.value=x
       self.head.next=None
     else:
-      current= self.head  
+      current= self.head
+      prev=self.head
       while(current is not None):
+        print(current.value, new.value)
         if current.value >= x:
-          new.prev=current.prev
+          prev.next=new
           new.next=current
+          new.prev=current.prev
           current.prev=new
 		  #if current.next is None:
-          return True 
-        print(current.value)
+          return True
+        prev=current
         current = current.next
-    return False	
+    return False
 
-  def remove(self):
-    print("Remove")
-    
+  def remove(self,x):
+    current= self.head
+    prev=self.head
+    print("Starting Deletion "),
+    while(current):
+      if current.value == x:
+        print("Found", current.value),
+        prev.next=current.next
+        current.next.prev =prev
+        return True
+      prev=current
+      current = current.next
+    print("Not Found", x),
+    return False
+
   def search(self, x):
     current= self.head
-    print("Starting Search "), 	
+    print("Starting Search "),
+    indx=0
     while(current):
-      print("->", current.value),
+      if current.value == x:
+        print("Found", current.value),
+        return indx
+      indx+=1
       current = current.next
-  
+    print("Not Found", x),
+    return False
+
   def display(self):
     current= self.head
-    print("Head"), 	
+    print("Head"),
     while(current):
       print("->", current.value),
       current = current.next
-  
 
-    
-    
+
+
+
 if __name__ =="__main__":
   ll=LL()
-  for i in range(1,4):
+  for i in range(1,10):
     ll.append(i)
-  for i in range(6,9):
+  for i in range(30,36):
     ll.append(i)
-  ll.insertAt(5)
+  for i in range(11,29):
+    ll.insertAt(i)
   ll.display()
-    
+  ll.search(12)
+  ll.remove(10)
+  ll.display()
